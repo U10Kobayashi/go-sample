1099ec6a044df8d6eb1f5d4e6759f9302de689c8
Yuto kobayashi <kobayuu0713@gmail.com>
2022-02-05 14:17:00 +0900

    get apiの実装

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+    "log"
+    "encoding/json"
+    "fmt"
+    "github.com/gorilla/mux"
+    "net/http"
+)
+
+type Book struct {
+    ID     string  `json:"id"`
+    Title  string  `json:"title"`
+    Author *Author `json:"author"`
+}
+
+type Author struct {
+    FirstName string `json:"firstname"`
+    LastName  string `json:"lastname"`
+}
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(books)
+}
+
+var books []Book
 
 func main() {
-    fmt.Println("Hello, World!")
+    r := mux.NewRouter()
+
+    books = append(books, Book{ID: "1", Title: "Book one", Author: &Author{FirstName: "Philip", LastName: "Williams"}})
+    books = append(books, Book{ID: "2", Title: "Book Two", Author: &Author{FirstName: "John", LastName: "Johnson"}})
+
+    r.HandleFunc("/api/books", getBooks).Methods("GET")
+    // r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+
+    fmt.Println("ok")
+
+    log.Fatal(http.ListenAndServe(":8080", r))
 }
